f7031dffa2005cf81cb46b0c12c9bb30a37eb259
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-03-15 21:02:31 +0100

    Add variable to disable the circle for the orbs.

--- scenes/game/orb/Orb.gd
+++ scenes/game/orb/Orb.gd
@@ -3,13 +3,15 @@ extends Node2D
 
 
 export var color: Color setget _set_color
+export var draw_orb = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _draw() -> void:
-	var circle_color = color
-	circle_color.a = 0.5
-	draw_circle(Vector2(), 6, circle_color)
+	if draw_orb:
+		var circle_color = color
+		circle_color.a = 0.5
+		draw_circle(Vector2(), 6, circle_color)
 
 
 func _set_color(new_color: Color):
