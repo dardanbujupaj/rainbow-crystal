1d4e6c0dd571d4ff739c7755ef58061e5566885c
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-03-15 21:03:11 +0100

    Add Ent with animation tree.

---
+++ scenes/game/forest/Ent.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+onready var animation_tree = $AnimationTree
+onready var state_machine = animation_tree["parameters/playback"]
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	yield(get_tree().create_timer(2), "timeout")
+	state_machine.travel("activate")
+	$Timer.start(5)
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func shake(amount: float) -> void:
+	get_tree().call_group("cameras", "add_trauma", amount)
+
+
+func _on_StompTimer_timeout() -> void:
+	state_machine.travel("stomp")
