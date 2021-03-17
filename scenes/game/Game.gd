a724ba8c6abe1e25bb757dfcea3e5b5e36b319bf
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-03-17 03:24:27 +0100

    Clean up game scene.

--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -1,26 +1,17 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+
+onready var shader: ShaderMaterial = $CanvasLayer/TextureRect.material
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-onready var shader: ShaderMaterial = $CanvasLayer/HSplitContainer/TextureRect.material
-
+# enable or disable a color
 func _set_color_enabled(enabled: bool, color: String) -> void:
 	shader.set_shader_param("%s_enabled" % color, enabled)
-	
 
+
+# load an area in the game
+# This is usually called by the Portal class
 func load_area(scene: PackedScene) -> void:
 	print("load area %s" % scene)
 	var instance = scene.instance()
