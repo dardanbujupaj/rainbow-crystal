49fa37d1c80e43337b02591842b965b569ed06d8
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-03-18 03:11:50 +0100

    Fix mushroom movement.

--- scenes/game/forest/Mushroom.gd
+++ scenes/game/forest/Mushroom.gd
@@ -7,22 +7,37 @@ const SPEED = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	
-	
-func _process(delta: float) -> void:
-	scale.x = direction
+	update_direction()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	
 	var collider = $RayCast2D.get_collider()
-	if collider is Character:
-		collider.hit(0.3, Vector2(direction, 0))
+	
 	if collider != null:
 		direction *= -1
-	move_and_slide(Vector2(direction * SPEED, 10), Vector2.UP)
+		update_direction()
+		
+		
+		if collider is Character:
+			collider.hit(0.3, Vector2(direction, 0))
+			$RayCast2D.collide_with_bodies = false
+			$AttackCooldown.start(1)
+		
+		
+	move_and_slide(Vector2(direction * SPEED, 200), Vector2.UP)
+
+
+func update_direction():
+	$AnimatedSprite.scale.x = direction
+	$RayCast2D.cast_to = Vector2(direction * 15, 0)
 
 
 func hit(damage, impact):
 	pass
+
+
+func _on_AttackCooldown_timeout() -> void:
+	$RayCast2D.collide_with_bodies = true
