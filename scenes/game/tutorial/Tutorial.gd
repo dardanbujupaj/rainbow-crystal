0ebac1a04d06a1e12c1bcc39903c9e3b020002ec
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-03-19 21:46:12 +0100

    Finish Dialog class.
    Add sample Dialog in Tutorial.

--- scenes/game/tutorial/Tutorial.gd
+++ scenes/game/tutorial/Tutorial.gd
@@ -1,38 +1,21 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
+onready var dialog = $CanvasLayer/Dialog
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	MusicEngine.play_song("Crystals")
-	
-	
-
-func set_red_disabled():
-	get_parent()._set_color_enabled(false, "red")
-	pass
-
-
-func set_green_disabled():
-	get_parent()._set_color_enabled(false, "green")
-	pass
-
-
-func set_blue_disabled():
-	get_parent()._set_color_enabled(false, "blue")
-	pass
-	
-	
-func orb_sound():
-	SoundEngine.play_sound("OrbWhoosh")
-
-
-func _on_Area2D_body_entered(body: Node) -> void:
 	MusicEngine.play_song("Runaway")
-	var state_machine = $AnimationTree["parameters/playback"]
-	state_machine.travel("orbs_disappearing")
-		
+	
+	dialog.queue_step({
+		"text_key": "The habitants of Prism, your home village, are in midst of the preparations for the festival of the crystal.",
+	})
+	dialog.queue_step({
+		"text_key": "You’ve been assigned to collect the apples with one of the village eldest."
+	})
+	dialog.queue_step({
+		"text_key": "Oy, hurry up. We need to collect those apples for the festival of the crystal!",
+		"speaker": "Old person"
+	})
+
--- util/Dialog.gd
+++ util/Dialog.gd
@@ -1,16 +1,83 @@
 extends Control
 
+signal next_step
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+"""
+class Signal:
+	var target: Node
+	var signal: String
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+class DialogStep:
+	var text_key: String
+	var speaker: String
+	var end_trigger: Signal
+	var end_hook: FuncRef
+"""
+
+
+var step_queue = []
+var current_step = null
+var characters_left: int = 0
+
+
+onready var label = $MarginContainer/RichTextLabel
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("g_interact"):
+		if characters_left == 0:
+			emit_signal("next_step")
+		else:
+			characters_left = 0
+
+
+func _process(delta: float) -> void:
+	render_current_step()
+	
+
+func queue_step(step) -> void:
+	step_queue.append(step)
+	if current_step == null:
+		next_step()
+
+
+func next_step() -> void:
+	show()
+	var step = step_queue.pop_front()
+	
+	if step != null:
+		current_step = step
+		characters_left = len(step["text_key"])
+		
+		render_current_step()
+		
+		if step.has("end_trigger"):
+			yield(step["end_trigger"]["target"], step["end_trigger"]["signal"])
+		else:
+			yield(self, "next_step")
+			
+		current_step = null
+		if step.has("end_hook"):
+			step["end_hook"].call_func()
+		
+		call_deferred("next_step") # I think this is a good idea for the call stack?
+	else:
+		hide()
+
+
+func render_current_step():
+	if current_step:
+		var content = current_step.text_key.substr(0, len(current_step.text_key) - characters_left)
+		
+		if current_step.has("speaker"):
+			content = "%s: %s" % [current_step.speaker, content]
+			label.bbcode_text = content
+		else:
+			content = "[color=#cccccc]%s[/color]" % [content]
+			label.bbcode_text = content
+
+
+func _on_CharacterTimer_timeout() -> void:
+	if characters_left > 0:
+		characters_left -= 1
