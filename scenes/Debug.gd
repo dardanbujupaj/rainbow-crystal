6d7d40a3752690194ec739cf265228399ef2fcdd
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2022-02-02 00:52:07 +0100

    Add translation plugin.
    Rename bosses.
    Add window size debug entry.

--- scenes/Debug.gd
+++ scenes/Debug.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 
 onready var fps := $FPS
+onready var resolution := $Resolution
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	fps.text = "%d FPS" % Performance.get_monitor(Performance.TIME_FPS)
+	
+	resolution.text = "%dx%d" % [OS.window_size.x, OS.window_size.y]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
